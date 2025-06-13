999a84f9e3403d621abcd5f1d189eccaf78a0ad1
Paul Lewis <lewi0231@flinders.edu.au>
2025-06-14 04:19:30 +0930

    fix(issue with dockerfile references)

--- apps/work-rate-calculator/next.config.js
+++
@@ -1,5 +0,0 @@
-// next.config.js
-module.exports = {
-  allowedDevOrigins: ["10.40.40.250"],
-  // ...other configurations
-};
---
+++ apps/work-rate-calculator/next.config.mjs
@@ -0,0 +1,2 @@
+// next.config.js
+export const allowedDevOrigins = ["10.40.40.250"];
