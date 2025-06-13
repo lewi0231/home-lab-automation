04e85ae4dad921a0748215a725c5616715258964
Paul Lewis <lewi0231@flinders.edu.au>
2025-06-13 11:52:34 +0930

    Some minor changes to blog and created work-rate-calculator app nextjs app.

--- apps/personal-blog/app/data.ts
+++ apps/personal-blog/app/data.ts
@@ -36,14 +36,6 @@ export const PROJECTS: Project[] = [
       'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/newProfileItem/d898be8a-7037-4c71-af0c-8997239b050d.mp4?_a=DATAdtAAZAA0',
     id: 'project1',
   },
-  {
-    name: 'Motion Primitives stuff',
-    description: 'UI kit to make beautiful, animated interfaces.',
-    link: 'https://motion-primitives.com/',
-    video:
-      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/XSfIvT7BUWbPRXhrbLed/ee6871c9-8400-49d2-8be9-e32675eabf7e.mp4?_a=DATAdtAAZAA0',
-    id: 'project2',
-  },
 ]
 
 export const WORK_EXPERIENCE: WorkExperience[] = [
